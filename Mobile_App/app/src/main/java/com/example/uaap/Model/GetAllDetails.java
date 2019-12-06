bab223af677469a151904835d5ea8e628608e5c0
jygahallon <jygahallon@student.apc.edu.ph>
2019-12-07 07:59:14 +0800

    Latest

---
+++ Mobile_App/app/src/main/java/com/example/uaap/Model/GetAllDetails.java
@@ -0,0 +1,112 @@
+package com.example.uaap.Model;
+
+public class GetAllDetails {
+    public int id;
+    public String time;
+    public String period;
+    public String callType;
+    public String committing;
+    public String disadvantage;
+    public String referee;
+    public String area;
+    public String areaOfPlay;
+    public String reviewDecision;
+    public String comment;
+    public String scores;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public String getCallType() {
+        return callType;
+    }
+
+    public void setCallType(String callType) {
+        this.callType = callType;
+    }
+
+    public String getCommitting() {
+        return committing;
+    }
+
+    public void setCommitting(String committing) {
+        this.committing = committing;
+    }
+
+    public String getDisadvantage() {
+        return disadvantage;
+    }
+
+    public void setDisadvantage(String disadvantage) {
+        this.disadvantage = disadvantage;
+    }
+
+    public String getReferee() {
+        return referee;
+    }
+
+    public void setReferee(String referee) {
+        this.referee = referee;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAreaOfPlay() {
+        return areaOfPlay;
+    }
+
+    public void setAreaOfPlay(String areaOfPlay) {
+        this.areaOfPlay = areaOfPlay;
+    }
+
+    public String getReviewDecision() {
+        return reviewDecision;
+    }
+
+    public void setReviewDecision(String reviewDecision) {
+        this.reviewDecision = reviewDecision;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getScores() {
+        return scores;
+    }
+
+    public void setScores(String scores) {
+        this.scores = scores;
+    }
+}
